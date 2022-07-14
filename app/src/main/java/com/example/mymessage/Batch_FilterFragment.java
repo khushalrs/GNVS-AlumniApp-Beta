38239b058b5e37dcce65c5b54e68b86eae41e379
rohitraval <rohitraval26112002@gmail.com>
2022-07-14 11:34:06 +0530

    Exp

--- app/src/main/java/com/example/mymessage/Batch_FilterFragment.java
+++ app/src/main/java/com/example/mymessage/Batch_FilterFragment.java
@@ -18,6 +18,7 @@ public class Batch_FilterFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    //Hello
 
     // TODO: Rename and change types of parameters
     private String mParam1;
