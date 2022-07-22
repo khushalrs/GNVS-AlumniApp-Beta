06fe016fc6994cbedc8df9d904877d3f180eb9fa
rohitraval <rohitraval26112002@gmail.com>
2022-07-22 11:40:37 +0530

    dfher7ggy75yg

--- app/src/main/java/com/example/mymessage/HomeFragment.java
+++ app/src/main/java/com/example/mymessage/HomeFragment.java
@@ -20,6 +20,8 @@ public class HomeFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
+    //eruhfuergygtg8t8g gfiyrgh5hg5
+
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
