d6cde0bea073100256a3a084756be86a6d3c915f
rohitraval <rohitraval26112002@gmail.com>
2022-07-25 11:47:14 +0530

    ProfileFragment ui

--- app/src/main/java/com/example/mymessage/ProfileFragment.java
+++ app/src/main/java/com/example/mymessage/ProfileFragment.java
@@ -1,18 +1,31 @@
 package com.example.mymessage;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.SuccessContinuation;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Objects;
+import java.util.concurrent.Executor;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,6 +37,9 @@ public class ProfileFragment extends Fragment {
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference users = database.getReference().child("users");
     DatabaseReference ref = users.child(Objects.requireNonNull(FirebaseAuth.getInstance().getUid()));
+    private FirebaseAuth mAuth;
+    Context context;
+    Button signout;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -62,13 +78,36 @@ public class ProfileFragment extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+            //tracking the sign in and singn out operations
+
+
         }
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View V = inflater.inflate(R.layout.fragment_profile, container, false);
+        signout = V.findViewById(R.id.SignOutbtn);
+        signout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonClicked();
+            }
+        });
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        context = Objects.requireNonNull(container).getContext();
+        return V;
+
+
+    }
+
+
+    public void buttonClicked(){
+
+
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(context,SignInActivity.class));
     }
 }
